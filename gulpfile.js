3bf112c8c3ebe58c61cd52f8a5371ec59c77e5ed
atyenoria <eigotyoubunn30@gmail.com>
2015-10-11 12:03:52 +0900

    minor

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,52 @@
 var gulp = require('gulp');
-var static = require('node-static');
 var browserSync = require("browser-sync");
-
+// var static = require('node-static');
 var bs1 = browserSync.create("proxy1");
-var bs2 = browserSync.create("proxy2");
+// var bs2 = browserSync.create("proxy2");
 
 
 
 gulp.task('browser-sync', function() {
 
 bs1.init({
-    proxy: "http://localhost:7070",
-    port: 5000,   // SATIC SERVER ACCESS!!
+    proxy: "http://localhost:3000",
+    port: 4000,   // MAIN SERVER ACCESS!!
     reloadDelay: 0,
         ui: {
         port: 9999
     }
 });
 
-bs2.init({
-    proxy: "http://localhost:3000", // MAIN SERVE PORT!!
-    port: 4000,  // MAIN SERVER ACCESS!!
-    reloadDelay: 0,
-            ui: {
-        port: 9998
-    }
-});
+// bs2.init({
+//     proxy: "http://localhost:3000", // MAIN SERVE PORT!!
+//     port: 4000,  // MAIN SERVER ACCESS!!
+//     reloadDelay: 0,
+//             ui: {
+//         port: 9998
+//     }
+// });
 
 });
 
 
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+    bs1.reload();
+    // bs2.reload();
 });
 
 
 
 
-gulp.task('node-static', function () {
-var file = new static.Server('../public');  // STATIC FILES!!
+// gulp.task('node-static', function () {
+// var file = new static.Server('../public');  // STATIC FILES!!
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-    file.serve(request, response);
-    }).resume();
-}).listen(7070);
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//     file.serve(request, response);
+//     }).resume();
+// }).listen(7070);
 
-});
+// });
 
 
 
@@ -55,6 +55,7 @@ require('http').createServer(function (request, response) {
 gulp.task('default', ['browser-sync','node-static'], function () {
     gulp.watch("../**/*", ['bs-reload']);
 });
+
 
 
 
